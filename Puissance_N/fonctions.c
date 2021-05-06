de7b28a9b573c9613227bcef9eff5eadc2881d55
bmwi3 <83547610+bmwi3@users.noreply.github.com>
2021-05-06 23:07:42 +0200

    Update fonctions.c

--- Puissance_N/fonctions.c
+++ Puissance_N/fonctions.c
@@ -15,7 +15,7 @@ void logo(void) {
     printf("|  |___|  |   ___|   ___| |   |  |   |  |___  |___    |       |                                             \n");
 };
 
-void vider_buffer(void)
+void vider_buffer(void) //permet de videer le buffer
 {
     int c;
 
@@ -24,4 +24,42 @@ void vider_buffer(void)
     } while (c != '\n' && c != EOF);
 }
 
+void action (){ // fonction qui permet de proposer les differentes actions possible au joueur
+    int c=0;
 
+    printf("Voulez-vous:\n1.Ajouter un jeton\n2.Enlever un jeton\n3.Quitter\n");
+    scanf("%d",&c);
+    do {
+        switch (c) {
+            case 1:
+                //appel de fonction add_token
+                printf("appel de fonction add_token\n");
+                break;
+            case 2:
+                //appel de fonction remove_token
+                printf("appel de fonction remove_token");
+                break;
+            case 3:
+                break;
+            default:
+                ;
+        }
+    } while (c!=1 && c!=2 && c!=3);
+
+}
+
+int add_token(char *grid, int indice_colonne, char *jeton){//verification colonne, verif jeton, modif colon return
+    if (indice_colonne<0 || indice_colonne> colonnemax || grid[indice_colonne-1][0]!=' '){
+        return 0;
+    } else{
+        if (jeton=='O'){
+            grid[indice_colonne-1][0]='O';
+            return 1;
+        }
+        else if (jeton=='X'){
+            grid[indice_colonne-1][0]='X';
+            return 1;
+        }
+    }
+
+}
