f482b63e8ccb66a1ced00322e9707f257d7eaa81
bmwi3 <83547610+bmwi3@users.noreply.github.com>
2021-05-06 21:54:52 +0200

    Update main.c

--- Puissance_N/main.c
+++ Puissance_N/main.c
@@ -3,9 +3,40 @@
 #include <ctype.h>
 #include <string.h>
 #include "fonctions.h"  //on inclut la librairie
+#include "fonctions.c"
 
-int main() {
+int main(int argc, char* argv[]) {
 logo();
-return 0;
+char choice;
+
+    do {
+        printf("Bienvenue sur le jeux Puissance N !\nNouvelle partie: N\nCharger une sauvegarde: S\nQuitter: Q\n");
+        scanf("%c",&choice);
+
+        choice=toupper(choice);
+
+        switch (choice) {
+            case 'N':
+                //appelfonctionnouvelle partie
+                printf("Nouvelle partie");
+                break;
+            case 'S':
+                //appelfonction charger sauvegarde
+                printf("Chargement en cours");
+                break;
+            case 'Q':
+                //quitter
+                printf("bye");
+                break;
+            default:
+        ;
+        }
+            vider_buffer(); //permet de liberer le charactère dans choice
+
+        } while (choice!='S'&& choice!='N'&& choice!='Q');
+
+
+
+return EXIT_SUCCESS;
 }
 
