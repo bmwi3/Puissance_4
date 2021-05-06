d2e1aa11e9cb307806bc1a2b55c757216798fc6a
bmwi3 <83547610+bmwi3@users.noreply.github.com>
2021-05-06 21:56:03 +0200

    Update fonctions.h

--- Puissance_N/fonctions.h
+++ Puissance_N/fonctions.h
@@ -2,11 +2,12 @@
 // Created by julie on 05/05/2021.
 //
 
+#pragma once
 #ifndef PUISSANCE_N_FONCTIONS_H
 #define PUISSANCE_N_FONCTIONS_H
 
 void logo(void);
+void vider_buffer(void);
 
 
 #endif //PUISSANCE_N_FONCTIONS_H
-
