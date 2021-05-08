109b40d152ed9f34ad52d84123f306348949385b
bmwi3 <83547610+bmwi3@users.noreply.github.com>
2021-05-08 22:16:24 +0200

    Update main.c

--- Puissance_N/main.c
+++ Puissance_N/main.c
@@ -5,6 +5,14 @@
 #include "fonctions.h"  //on inclut la librairie
 #include "fonctions.c"
 
+
+
+typedef struct{
+    int hauteur;
+    int largeur;
+    char grille;
+}Grid;
+
 int main(int argc, char* argv[]) {
 logo();
 char choice;
@@ -36,6 +44,24 @@ char choice;
             vider_buffer(); //permet de liberer le charactère dans choice
 
         } while (choice!='S'&& choice!='N'&& choice!='Q');
+    
+      int N;
+    Grid g1;
+    printf("entrer la valeur de N+2:\n");
+    scanf("%d",&N);
+    g1.largeur = N;
+    g1.hauteur = N;
+
+    g1.grille = (char) malloc(sizeof (char)g1.largeur);
+    for (int i=0;i<g1.largeur;i++){
+        g1.grille[i]=(char) malloc(sizeof (char)g1.hauteur);
+        for (int j=0;j<g1.hauteur;j++){
+            g1.grille[i][j]='_';
+        }
+    }
+
+    show_grid(g1.grille,g1.largeur,g1.hauteur);
+
 
 
 
