18c81a9d9922f89c6ef496e67c961c7e745a99bc
bmwi3 <83547610+bmwi3@users.noreply.github.com>
2021-05-18 08:59:06 +0200

    Update fonctions.h

--- Puissance_N/fonctions.h
+++ Puissance_N/fonctions.h
@@ -6,21 +6,23 @@
 #ifndef PUISSANCE_N_FONCTIONS_H
 #define PUISSANCE_N_FONCTIONS_H
 
-typedef struct{
-    int hauteur;
-    int largeur;
-    char** grille;
-}Grid;
-
 void logo(void);
 void vider_buffer(void);
 void action ();
-int add_token(char *grid, int indice_colonne, char *jeton);
-void show_grid(char** grille, int largeur, int hauteur);
-int check_winner(char **grid, int n);
+int add_token(char *grid, int indice_colonne, char *jeton,int colonne_max);
+int remove_token(char *grid, int indice_colonne, char *jeton,int colonne_max);
+void show_grid(char** grille,int largeur, int hauteur);
+int remove_token_bis(char**grid, int colonne,int largeur);
+int add_token_bis(char**grid,char jeton, int colonne,int largeur);
+
+typedef struct{
+    int hauteur;
+    int largeur;
+    char **grille;
+}Grid;
+
   
 
-    
-
+   
 
 #endif //PUISSANCE_N_FONCTIONS_H
