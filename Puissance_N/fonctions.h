d0b0f14dad529081087f84068fc209c115230067
Leonard-hub-wq <83727577+Leonard-hub-wq@users.noreply.github.com>
2021-05-20 13:43:08 +0200

    Update fonctions.h

--- Puissance_N/fonctions.h
+++ Puissance_N/fonctions.h
@@ -6,6 +6,14 @@
 #ifndef PUISSANCE_N_FONCTIONS_H
 #define PUISSANCE_N_FONCTIONS_H
 
+
+typedef struct{
+    int hauteur;
+    int largeur;
+    char **grille;
+}Grid;
+
+
 void logo(void);
 void vider_buffer(void);
 void action ();
@@ -14,12 +22,6 @@ int remove_token(char *grid, int indice_colonne, char *jeton,int colonne_max);
 void show_grid(Grid g1);
 int remove_token_bis(char**grid, int colonne,int largeur);
 int add_token_bis(char**grid,char jeton, int colonne,int largeur);
-
-typedef struct{
-    int hauteur;
-    int largeur;
-    char **grille;
-}Grid;
 
   
 
