b62e01a16d86707e2f54729afabb8d1425b394a3
unainposteur <83547907+unainposteur@users.noreply.github.com>
2021-05-20 16:38:02 +0200

    Create main 1.2.c

---
+++ Puissance_N/main 1.2.c
@@ -0,0 +1,34 @@
+int main () {
+  int n, nombre_de_joueur;
+  
+ //printf le logo
+  
+ printf("Bienvenue au puissance N /nNouvelle partie : 1 /nPartie chargée : 2 /nQuitter : 0/n");
+ 
+ do 
+   scanf("%d",start);
+   if (start != ( 0 || 1 || 2)){
+      printf("Impossible, choisir 0, 1 ou 2");
+     }
+ while (start != ( 0 || 1 || 2))}
+   
+ switch( start) ;
+  
+  cas 1:
+    printf("combien de joueur: 1 ou 2/n");
+    scanf("%d",nombre_de_joeur);
+    printf("nombre de jeton à assembler/n");
+    scanf("%d",n);
+  break;
+  
+  cas 2:
+    //fonction load partie chargé
+  break;
+  
+  cas 3:
+    return 0;
+    break ;
+  
+  
+  
+}
