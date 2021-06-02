6e704470f11356f430cdf16399a1c387a7d920ed
Leonard-hub-wq <83727577+Leonard-hub-wq@users.noreply.github.com>
2021-06-02 17:00:38 +0200

    Update grid.c

--- projet_bis/grid.c
+++ projet_bis/grid.c
@@ -11,24 +11,24 @@
 #include "grid.h"
 void initialise(Grid *g1){
 
-    g1->grille = (char**) malloc(sizeof (char*) * g1->largeur);
+    g1->grille = (char**) malloc(sizeof (char*) * g1->largeur); //on impémente une largeur a notre tableau de charactère g1
     for (int i=0;i<g1->largeur;i++){
-        g1->grille[i]=(char*) malloc(sizeof (char) * g1->hauteur);
+        g1->grille[i]=(char*) malloc(sizeof (char) * g1->hauteur); //de meme on lui implémente une hauteur
         for (int j=0;j<g1->hauteur;j++){
-            g1->grille[i][j] = ' ';
+            g1->grille[i][j] = ' '; //pour toute valeur du tableau, on initialise a "_"
         }
     }
 }
 
 void show_grid(Grid g1){
-    for(int k=0;k<g1.largeur;k++){
+    for(int k=0;k<g1.largeur;k++){ //on numérote les colonnes du tableau pour facilité la saisi a l'utilisateur
         printf(" %d ",k+1);
 
     }
     printf("\n");
-    for (int i=0;i<g1.largeur;i++){
+    for (int i=0;i<g1.largeur;i++){ 
         for (int j=0;j<g1.hauteur;j++){
-            printf("{%c}",g1.grille[i][j]);
+            printf("{%c}",g1.grille[i][j]);  //on affiche pour chaque case du tableau le charactère qu'elle contiens
 
         }
         printf("\n");
