077507be874d5e2637e088daeb5d92cf0cbeb3f3
unainposteur <83547907+unainposteur@users.noreply.github.com>
2021-06-04 21:49:51 +0200

    Update fonction_check.c

--- porjet_flinal/fonction_check.c
+++ porjet_flinal/fonction_check.c
@@ -7,39 +7,39 @@ int trouver_x (Grid g1, int colonne, char jeton ){  // cette fonction sert a tro
     return x;
 }
 
-int check_colonne (Grid g1, int colonne, char jeton, int x){         // cette fonction sert a regarder si le dernier jeton est aligner avec n jeton sur la colonne
-    int y = colonne - 1,compteur_colonne = 1;                        // compteur_colonne est le nombre de jeton aligner en colonne
-    int c = 1,j = 0;                                                 // c est une varible qui donne l'information si le jeton en dessous est identique ou non
+int check_colonne (Grid g1, int colonne, char jeton, int x){        // cette fonction sert a regarder si le dernier jeton est aligner avec n jeton sur la colonne
+    int y = colonne - 1,compteur_colonne = 1;                       // compteur_colonne est le nombre de jeton aligner en colonne
+    int c = 1,j = 0;                                                // c est une varible qui donne l'information si le jeton en dessous est identique ou non
 
-    do{                                                              // on vérifie si le jeton du dessous est différent ou si on a déja n jeton aligner
+    do{                                                             // on vérifie si le jeton du dessous est différent ou si on a déja n jeton aligner
         j++;
-        if (x+j > g1.largeur-1){                                     // on regarde si le jeton en dessous est bien dans le tableau
+        if (x+j > g1.largeur-1){                                    // on regarde si le jeton en dessous est bien dans le tableau
 
-            c = 0;                                                   // le jeton est dessous est hors de la grille
-        }else if (g1.grille[x + j][y] != jeton ){                    // on compare le dernier jeton jouer avec celui d'en dessous
-            c = 0;                                                   // le jeton en dessous est différent
+            c = 0;                                                  // le jeton est dessous est hors de la grille
+        }else if (g1.grille[x + j][y] != jeton ){                   // on compare le dernier jeton jouer avec celui d'en dessous
+            c = 0;                                                  // le jeton en dessous est différent
         }
-        compteur_colonne += c ;                                      // le compteur de colonne prrend 1 si jeton d'en dessous identique et 0 sinon
+        compteur_colonne += c ;                                     // le compteur de colonne prrend 1 si jeton d'en dessous identique et 0 sinon
         
     } while (c != 0 && compteur_colonne != g1.largeur-2);    
 
-    return compteur_colonne;                                         // on retourne le nombre de jeton aligné en colonne
+    return compteur_colonne;                                        // on retourne le nombre de jeton aligné en colonne
 }
 
-int check_ligne (Grid g1, int colonne, char jeton,int x){            //compte le nombre de jeton aligner en ligne
+int check_ligne (Grid g1, int colonne, char jeton,int x){           //compte le nombre de jeton aligner en ligne
     int y = colonne - 1,j = 0,compteur_ligne = 1;
     int ld = 1, lg = 1;
 
-    do{                                                              // on vérifie si les jetons de droite et de gauche sont différent du derniere pauser ou si on a déja n jeton aligner
+    do{                                                             // on vérifie si les jetons de droite et de gauche sont différent du derniere pauser ou si on a déja n jeton aligner
         j++;
-        if (g1.largeur -1 < y +j){                                   // on regarde si on se sort pas de la grille a droite
+        if (g1.largeur -1 < y +j){                                  // on regarde si on se sort pas de la grille a droite
             ld = 0;
-        } else if (g1.grille[x][y+ j] != jeton ){                    // on compare le dernier jeton jouer avec ceux de sa droite
+        } else if (g1.grille[x][y+ j] != jeton ){                   // on compare le dernier jeton jouer avec ceux de sa droite
             ld = 0;
         }
 
 
-        if ( y - j < 0){                                            // on regarde si on se sort pas de la grille a gauche
+        if ( y - j < 0){                                            // on regarde si on ne sort pas de la grille a gauche
             lg = 0;
         }else if (g1.grille[x][y- j] != jeton ){                    // on compare le dernier jeton jouer avec ceux de sa gauche
             lg = 0;
@@ -60,67 +60,73 @@ int check_diagonaled (Grid g1, int colonne, char jeton, int x){     // cette fon
         
         j++;
         
-        if (x+j > g1.largeur-1){                                    //on regarde si on se sort pas de la grille en bas
+        if (x+j > g1.largeur-1){                                    //on regarde si on ne sort pas de la grille en bas
             bd = 0;
-        }else if (g1.largeur -1 < y +j ){                           //on regarde si on se sort pas de la grille a droite
+        }else if (g1.largeur -1 < y +j ){                           //on regarde si on ne sort pas de la grille a droite
             bd = 0;
         }else if (g1.grille[ j+ x ][y + j] != jeton ){              // on compare le dernier jeton jouer avec celui en bas a droite
             bd = 0;
         }
 
-        if (x - j<0){                                               //on regarde si on se sort pas de la grille en haut
+        if (x - j<0){                                               //on regarde si on ne sort pas de la grille en haut
             hg = 0;
-        } else if ( y - j < 0) {                                    //on regarde si on se sort pas de la grille a gauche
+        } else if ( y - j < 0) {                                    //on regarde si on ne sort pas de la grille a gauche
             hg = 0;
         }else if (g1.grille[x- j][y- j] != jeton ){                 // on compare le dernier jeton jouer avec celui en haut a gauche
             hg = 0;
         }
 
-        compteur_diagonaled += hg + bd; // compteur de jeton aligner dans la premiere diagonale
-
+        compteur_diagonaled += hg + bd;                             // on compte le nombre de jeton aligner
     }while(hg + bd != 0&& compteur_diagonaled!= g1.largeur-2);
     
     return compteur_diagonaled;                                     // on retourne le nombre de jeton aligner en diagonale
 }
 
-int check_diagonaleg (Grid g1, int colonne, char jeton, int x){
+int check_diagonaleg (Grid g1, int colonne, char jeton, int x){     // cette fonction sert a regarder si le dernier jeton est aligner avec n jeton sur la diagonale
     int y = colonne - 1,j = 0,compteur_diagonaleg = 1;
     int bg = 1, hd = 1;
-    do {
+   
+    do {                                                            // on vérifie si les jeton dans l'autre diagonale sont different du duernier jeton poser ou qu'il y est déja n jeton aligner
+        
         j++;
-        if (x+j > g1.largeur-1){
+        
+        if (x+j > g1.largeur-1){                                    //on regarde si on ne sort pas de la grille en bas
             bg = 0;
-        }else if ( y - j < 0) {
+        }else if ( y - j < 0) {                                     //on regarde si on ne sort pas de la grille a gauche
             bg = 0;
-        }else  if (g1.grille[x+ j][y- j] != jeton ){ // on compare le dernier jeton jouer avec celui en bas a gauche
+        }else  if (g1.grille[x+ j][y- j] != jeton ){                // on compare le dernier jeton jouer avec celui en bas a gauche
             bg = 0;
         }
 
 
-        if (x - j<0){
+        if (x - j<0){                                               //on regarde si on ne sort pas de la grille en haut
             hd = 0;
-        }else if (g1.largeur -1 < y +j ){
+        }else if (g1.largeur -1 < y +j ){                           //on regarde si on ne sort pas de la grille a droite
             hd = 0;
-        }else if (g1.grille[x- j][y+ j] != jeton ){ // on compare le dernier jeton jouer avec celui en haut a droite
+        }else if (g1.grille[x- j][y+ j] != jeton ){                 // on compare le dernier jeton jouer avec celui en haut a droite
             hd = 0;
         }
 
-        compteur_diagonaleg += hd + bg; // compteur de jeton aligner dans la premiere diagonale
+        compteur_diagonaleg += hd + bg;                             // on compte le nombre de jeton aligner
 
 
     }while(hd + bg != 0 && compteur_diagonaleg != g1.largeur-2);
-    return compteur_diagonaleg;
+    
+    return compteur_diagonaleg;                                     // on retourne le nombre de jeton aligner en diagonale
+}
 }
 
-int  check_winner(Grid g1, int colonne,char jeton) {
+int  check_winner(Grid g1, int colonne,char jeton) {                // cette fonction sert a regarder si n jeton sont aligner en ligne, colonne ou en diagonale
     int x  = trouver_x ( g1, colonne, jeton );
 
 
 
     if (check_ligne (g1,colonne,jeton,x) == g1.largeur - 2 ||  check_colonne (g1, colonne, jeton, x) == g1.largeur - 2 || check_diagonaled (g1, colonne, jeton, x) == g1.largeur - 2 || check_diagonaleg (g1, colonne, jeton, x) == g1.largeur - 2) {
+                                                                    // on regarder si les fonction qui compte le nombre de jeton aligner retourne n 
         if (jeton == 'O') {
             printf("joueur 1 gagne bravo vous avez gagner\n");
             return 0;
+            
         } else {
             printf("joueur 2 gagne bravo vous avez gagner\n");
             return 1;
@@ -129,28 +135,32 @@ int  check_winner(Grid g1, int colonne,char jeton) {
     return -1;
 }
 
-int check_grille_pleine (Grid g1,int colonne,char jeton){
+int check_grille_pleine (Grid g1,int colonne,char jeton){           // cette fonction sert a regarder si la grille est complétement remplis
     int compteur_egalite = 0;
     int rejouer;
     int x = trouver_x ( g1, colonne, jeton );
-    if (x==0) { //regarde si il y a egalite
-        do {
-            if (g1.grille[0][compteur_egalite] != ' ') {
+    
+    if (x==0) {                                                     // regarde si le dernier jeton est dans la ligne du haut
+        do {                                                        // on compte le nombre de jeton qu'il y a dans la ligne du haut
+            if (g1.grille[0][compteur_egalite] != ' ') {            // à chaque fois qu'une case de la ligne du haut n'est pas vide on incrémente
                 compteur_egalite++;
             }
 
-        } while (g1.grille[0][compteur_egalite] != ' ');
+        } while (g1.grille[0][compteur_egalite] != ' '&& compteur_egalite !=g1.largeur);            
 
-        if (compteur_egalite == g1.largeur ) {
+        if (compteur_egalite == g1.largeur ) {                      
+            
             printf("grille pleine voulait vous continué ?\n1 pour continuer\n2 pour quitter\n");
             do {
+                
                 scanf("%d", &rejouer);
-                if (rejouer != 1 || rejouer != 2 ){
+                if (rejouer != 1 && rejouer != 2 ){
 
                     printf("Impossible, choisir 1 ou 2/n\n");
                 }
             }while (rejouer != 1 && rejouer != 2 );
-            if (rejouer == 1 ){
+            
+            if (rejouer == 2 ){
                 return 1;
             }
         }
