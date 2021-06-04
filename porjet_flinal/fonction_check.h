bc821327582f15cee09fba7894d7d6413b09c955
unainposteur <83547907+unainposteur@users.noreply.github.com>
2021-06-04 21:57:10 +0200

    Update fonction_check.h

--- porjet_flinal/fonction_check.h
+++ porjet_flinal/fonction_check.h
@@ -1,1 +1,9 @@
 
+int check_winner (Grid g1,int colonne, char jeton);
+int check_colonne (Grid g1, int colonne, char jeton, int x);
+int check_ligne (Grid g1, int colonne, char jeton,int x);
+int check_diagonaled (Grid g1, int colonne, char jeton,int x);
+int check_diagonaleg (Grid g1, int colonne, char jeton,int x);
+int check_grille_pleine (Grid g1,int colonne, char jeton);
+int check_ligne_bas_vide(Grid g1);
+int trouver_x (Grid g1, int colonne, char jeton );
