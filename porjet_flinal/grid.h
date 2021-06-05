eda969ab337b678ec3b7628dcd46e23bcdacc2b4
Leonard-hub-wq <83727577+Leonard-hub-wq@users.noreply.github.com>
2021-06-05 10:11:08 +0200

    Update grid.h

--- porjet_flinal/grid.h
+++ porjet_flinal/grid.h
@@ -9,7 +9,19 @@ typedef struct{
     int largeur;
     char **grille;
 }Grid;
+
+/**
+* affiche une grille avec les charactère qu'elle contiens
+* @param structure Grid qui contiens une matrice carré ainsi que ses dimentions 
+* @return rien
+*/
 void show_grid(Grid g1);
+
+/**
+* initialise toute les case d'une grille avec le caractere '_'
+* @param structure Grid passé par adresse 
+* @return rien
+*/
 void initialise(Grid *g1);
 
 #endif //PROJET_BIS_GRID_H
