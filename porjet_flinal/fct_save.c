c6350ae728bbf266a7c9307612b4aad0be4975ca
bmwi3 <83547610+bmwi3@users.noreply.github.com>
2021-06-06 15:31:43 +0200

    Update fct_save.c

--- porjet_flinal/fct_save.c
+++ porjet_flinal/fct_save.c
@@ -8,13 +8,13 @@
 
 
 
-int save(Grid g1,int i,char *joueur1,char *joueur2) {
-    FILE*fptr;
-    char array[( g1.largeur - 1)];
-    fptr = fopen("save.txt", "w");
-    if (fptr == NULL) {//on affiche un message d'erreur si on rencontre un probleme lors de la sauvegarde
-        printf(" File named save.txt does not exist or cannot be opened.\n");
-        return 0;
+int save(Grid g1,int i,char *joueur1,char *joueur2) { //sauvegarde la partie
+    FILE*fptr;                                        // pointeur sur le fichier
+    char array[( g1.largeur - 1)];                     // on declare un tableau
+    fptr = fopen("save.txt", "w");                      // on ouvre le fichier en mode ecriture
+    if (fptr == NULL) {                                 //on affiche un message d'erreur si on rencontre un probleme lors de la sauvegarde
+        printf(" File named save.txt does not exist or cannot be opened.\n");   //message d'erreur si problème
+        return 0;// on retourne 0 si il y a eu un problème
     } else {
 
         fprintf(fptr, "%d %d\n",i,g1.hauteur);//on ecrit dans le fichier save.txt la valeur de i, de la taille du tableau et les noms des deux joueurs
@@ -22,21 +22,21 @@ int save(Grid g1,int i,char *joueur1,char *joueur2) {
     }
     for (int i = 0; i < g1.largeur; i++)//on ecrit dans le fichier save.txt les valeur du tableau ligne par ligne
     {
-        strcpy(array, g1.grille[i]);
+        strcpy(array, g1.grille[i]); //on copie la ligne i de notre grille dans notre tableau 
 
-        fputs(array, fptr);
-        fputs("\n", fptr);
+        fputs(array, fptr); //on enregistre sur le fichier la premiere ligne qui est notre chaine de caractère a l'aide de fput
+        fputs("\n", fptr);//on saute une ligne pour respecter la mise en forme du tableau, pour aider à recuperer les infos
     }
-    fclose(fptr);
-    return 1;
+    fclose(fptr); //on ferme le fichier pour eviter les erreurs
+    return 1; //on retourne 1 si tout c'est bien passé
 }
 
-int load(FILE*fptr,Grid *g1,int *colonne,char *joueur1,char *joueur2){
-    int i;
-    fptr=fopen("save.txt","r");
+int load(FILE*fptr,Grid *g1,int *colonne,char *joueur1,char *joueur2){//fonction qui recupere un fichier enregistrer
+    int i; //on déclare un compteur i
+    fptr=fopen("save.txt","r"); //on ouvre notre fichier en mode lecture
     if(fptr==NULL) {//on affiche un message d'erreur si le fichier save.txt n'existe pas ou qu'il n'a pas pu etre chargé
-        printf(" File named save.txt does not exist or cannot be opened.\n");
-        return 0;
+        printf(" File named save.txt does not exist or cannot be opened.\n");//controle avec message d'erreur
+        return 0;//on retourne 0 si il y a eu un problème
     }
     else {
         fscanf(fptr,"%d %d\n",&i,colonne);//on lit dans le fichier le jouer dont c'est le tour(i) la taille du tableau et le nom des deux joueurs
@@ -46,13 +46,13 @@ int load(FILE*fptr,Grid *g1,int *colonne,char *joueur1,char *joueur2){
         initialise(g1);//on initialise un tableau de la taille prise dans le fichier et un lui incrémente les valeurs du tableau lu dans le fichier
         for (int j = 0; j < g1->largeur; j++){
             for (int k = 0; k < g1->largeur; k++){
-                fscanf(fptr,"%c",&g1->grille[j][k]);
+                fscanf(fptr,"%c",&g1->grille[j][k]);//on implemente le tableau caractère par caractère
 
 
             }
-            fscanf(fptr,"\n");
+            fscanf(fptr,"\n");//on saute une ligne pour que le tableau s'affiche ligne par ligne.
         }
-        fclose(fptr);
+        fclose(fptr);//on ferme le fichier pour eviter les erreurs
 
 
 
