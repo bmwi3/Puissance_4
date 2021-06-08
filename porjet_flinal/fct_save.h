54b567d73e2a589580b82d9c4706aff84bbe5e42
Leonard-hub-wq <83727577+Leonard-hub-wq@users.noreply.github.com>
2021-06-08 10:12:59 +0200

    Update fct_save.h

--- porjet_flinal/fct_save.h
+++ porjet_flinal/fct_save.h
@@ -24,5 +24,5 @@ int save(Grid g1,int i,char *joueur1,char *joueur2);
 * @param le nom des joueurs 1 et  2
 * @return le nombe i pour savoir a qui c'est de jouer ou 0 si le le fichier n'existe pas
 */
-int load(FILE*fptr,Grid *g1,int *colonne,char *joueur1,char *joueur2);
+int load(FILE*fptr,Grid *g1,int colonne,char *joueur1,char *joueur2);
 #endif //PROJET_BIS_FCT_SAVE_H
