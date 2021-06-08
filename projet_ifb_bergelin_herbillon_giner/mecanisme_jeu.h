4c749cec1e614ff86f60faefb7553fc9d1ff1c4c
unainposteur <83547907+unainposteur@users.noreply.github.com>
2021-06-08 20:01:59 +0200

    Update mecanisme_jeu.h

--- projet_ifb_bergelin_herbillon_giner/mecanisme_jeu.h
+++ projet_ifb_bergelin_herbillon_giner/mecanisme_jeu.h
@@ -6,7 +6,7 @@
 #define PROJET_IFB_BERGELIN_HERBILLON_GINER_MECANISME_JEU_H
 #include "grid.h"
 
-int IA (Grid g1,char jeton, char colonne);
+int IA (Grid g1,char jeton);
 int add_token_bis(Grid g1,char jeton,int colonne);
 int remove_token(Grid g1,char jeton, int colonne);
 #endif //PROJET_IFB_BERGELIN_HERBILLON_GINER_MECANISME_JEU_H
