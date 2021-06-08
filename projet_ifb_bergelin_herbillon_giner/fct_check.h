a13332fdfa37b83551c0d21e81cf31ed7efd5dc3
unainposteur <83547907+unainposteur@users.noreply.github.com>
2021-06-08 19:47:08 +0200

    Update fct_check.h

--- projet_ifb_bergelin_herbillon_giner/fct_check.h
+++ projet_ifb_bergelin_herbillon_giner/fct_check.h
@@ -11,8 +11,8 @@ int check_colonne (Grid g1, int colonne, char jeton, int x);
 int check_ligne (Grid g1, int colonne, char jeton,int x);
 int check_diagonaled (Grid g1, int colonne, char jeton,int x);
 int check_diagonaleg (Grid g1, int colonne, char jeton,int x);
-int check_grille_pleine (Grid g1,int colonne);
+int check_grille_pleine (Grid g1);
 int check_ligne_bas_vide(Grid g1);
 int trouver_x (Grid g1, int colonne);
-int check_egalite (Grid g1,int colonne);
+int check_egalite (Grid g1);
 #endif //PROJET_IFB_BERGELIN_HERBILLON_GINER_FCT_CHECK_H
