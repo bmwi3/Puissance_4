4fa654e065eed3deb25e45f709f19fa4edb65e10
bmwi3 <83547610+bmwi3@users.noreply.github.com>
2021-06-09 15:17:30 +0200

    Update mecanisme_jeu.h

--- projet_ifb_bergelin_herbillon_giner/mecanisme_jeu.h
+++ projet_ifb_bergelin_herbillon_giner/mecanisme_jeu.h
@@ -6,7 +6,29 @@
 #define PROJET_IFB_BERGELIN_HERBILLON_GINER_MECANISME_JEU_H
 #include "grid.h"
 
+/**
+* joue automatiquement tour après tour soit en posant ou en reitrant un jeton
+* @param Structure Grid (g1) avec la grille de jeu et sa taille
+* @param le caractère jeton pour que l'ia place et retire le bon jeton
+* @return b pour pouvoir controler si il y a un vainqueur grâce à la fonction check_winner
+*/
 int IA (Grid g1,char jeton);
+
+/**
+* permet d'ajouter un jeton dans la colonne choisit
+* @param Structure Grid (g1) avec la grille de jeu et sa taille
+* @param le caractère jeton pour placer le bon jeton 
+* @param l'entier colonne pour savoir dans quelle colonne placer le jeton
+* @return 0 si la colonne choisie ne remplit pas les conditions et return 1 si tout c'est bien passé
+*/
 int add_token_bis(Grid g1,char jeton,int colonne);
+
+/**
+* permet de retirer un jeton dans la colonne choisit
+* @param Structure Grid (g1) avec la grille de jeu et sa taille
+* @param le caractère jeton pour retirer le bon jeton
+* @param l'entier colonne pour savoir dans quelle colonne retirer le jeton
+* @return 0 si la colonne choisie ne remplit pas les conditions et return 1 si tout c'est bien passé
+*/
 int remove_token(Grid g1,char jeton, int colonne);
 #endif //PROJET_IFB_BERGELIN_HERBILLON_GINER_MECANISME_JEU_H
