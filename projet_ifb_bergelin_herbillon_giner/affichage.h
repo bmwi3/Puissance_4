b7a42d98b95493c38a223b1816cdcb818c2882b5
bmwi3 <83547610+bmwi3@users.noreply.github.com>
2021-06-09 15:20:38 +0200

    Update affichage.h

--- projet_ifb_bergelin_herbillon_giner/affichage.h
+++ projet_ifb_bergelin_herbillon_giner/affichage.h
@@ -7,7 +7,16 @@
 #include "grid.h"
 #include "fct_check.h"
 
+/**
+* Permet d'afficher le logo puissance N
+*/
 void logo(void);
+
+/**
+* permet de présenter et de récupérer les choix de l'utilisateur tour après tour
+* @param Structure Grid (g1) avec la grille de jeu et sa taille
+* @return choice pour être utilisé dans le switch du main qui permet de lancer les différentes fonctions.
+*/
 int option_choice(Grid g1);
 
 #endif //PROJET_IFB_BERGELIN_HERBILLON_GINER_AFFICHAGE_H
